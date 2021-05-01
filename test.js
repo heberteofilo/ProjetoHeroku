3ebb2cca9e07b2dc1c340fe7c43ac8ea4eaf2c15
Heber Dev <heberteofilo123@gmail.com>
2021-05-01 10:57:05 -0300

    att

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const got = require('got');
 const test = require('tape');
 
 // Start the app
+// Iniciar o app
 const env = Object.assign({}, process.env, {PORT: 5000});
 const child = spawn('node', ['index.js'], {env});
 
@@ -10,15 +11,20 @@ test('responds to requests', (t) => {
   t.plan(4);
 
   // Wait until the server is ready
+  // Esperar até o servidor funcionar
   child.stdout.on('data', _ => {
     // Make a request to our app
+    // Fazer a requisição para nosso app
     (async () => {
       const response = await got('http://127.0.0.1:5000');
       // stop the server
+      // parar o servidor
       child.kill();
       // No error
+      // Sem erro
       t.false(response.error);
       // Successful response
+      // resposta com sucesso
       t.equal(response.statusCode, 200);
       // Assert content checks
       t.notEqual(response.body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
